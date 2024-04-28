54baad8bfeb9c4bc6320ea501f9c34ff8eabf2bf
Reizeron <40732908+Reizeron@users.noreply.github.com>
2024-04-28 22:53:21 +0300

    Quartz sync: Apr 28, 2024, 10:53 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,7 +6,7 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
-  const iconPath = `/static/icon.png`
+  const iconPath = `${baseDir}/static/icon.png`
   return (
     <h1 class={classNames(displayClass, "page-title")}>
 	  <img src={iconPath}></img>
