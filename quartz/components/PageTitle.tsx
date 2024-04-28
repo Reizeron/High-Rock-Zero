ec914c6101f16b2af45819dc4b3344e3ae31ae40
Reizeron <40732908+Reizeron@users.noreply.github.com>
2024-04-28 16:32:39 +0300

    Quartz sync: Apr 28, 2024, 4:32 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,7 +8,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={classNames(displayClass, "page-title")}>
-	  <img src="../static/icon.png"></img>
+	  <img src="./static/icon.png"></img>
       <a href={baseDir}>{title}</a>
     </h1>
   )
